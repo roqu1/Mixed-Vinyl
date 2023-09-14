30e278190d24d25e1d30057fba70d3a535d5bc03
Corneliu Rusu <snipklas@gmail.com>
2023-09-14 10:47:06 +0200

    Added some Blocks

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -4,17 +4,29 @@ namespace App\Controller;
 
 // use the Route class from the Symfony\Component\Routing\Annotation namespace
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-
-        return new Response('Homepage');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'saySomething' => "Something",
+            'tracks' => $tracks,
+        ]);
     }
     // q: create a new route for the homepage at /.
     #[Route('/browse/{slug}')]
